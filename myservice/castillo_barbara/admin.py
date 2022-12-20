b357425c8fffa41bf1e6a51a8549e6f6b7c2d0fe
Baaaar4 <108684289+Baaaar4@users.noreply.github.com>
2022-12-20 19:41:07 -0300

    se borro todo
    todo

--- myservice/castillo_barbara/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from castillo_barbara.models import Idea, Pensamiento
-# Register your models here.
-
-admin.site.register(Idea)
-admin.site.register(Pensamiento)
--- myservice/castillo_barbara/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CastilloBarbaraConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'castillo_barbara'
-
